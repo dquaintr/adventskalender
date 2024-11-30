ee2028cdb5742026801f8bbd46f5837cbc9475a7
DanQua <daniel.quaintrell@web.de>
2024-11-30 17:32:47 +0100

    Add pic_dic dictionary

---
+++ venv/pic_dic.py
@@ -0,0 +1,24 @@
+'''
+This file is to create the dictionary. It has the day as the key and as value
+the relative path to the picture for that day. As stated in the readme file,
+the pictures were downloaded from pixabay.com and are free to use. The name of the author
+can be taken from the file name.
+Shout out to all the fotographers who made these good pictures.
+'''
+
+pic_dic= {
+    "01": "venv/xmas_pics/1_pexels.jpg", "02": "venv/xmas_pics/2_larisa_koshkina.jpg",
+    "03": "venv/xmas_pics/3_bruno.jpg", "04": "venv/xmas_pics/4_ylanite_koppens.jpg",
+    "05": "venv/xmas_pics/5_anja_steuerliese.jpg", "06": "venv/xmas_pics/6_ylanite_koppens.jpg",
+    "07": "venv/xmas_pics/7_normunds_ispwich.jpg", "08": "venv/xmas_pics/8_monika.jpg",
+    "09": "venv/xmas_pics/9_congerdesign.jpg", "10": "venv/xmas_pics/10_irina.jpg",
+    "11": "venv/xmas_pics/11_vincent_groeneveld.jpg", "12":"venv/xmas_pics/12_couleur.jpg",
+    "13": "venv/xmas_pics/13_jill_wellington.jpg", "14" : "venv/xmas_pics/14_no_author.jpg",
+    "15": "venv/xmas_pics/15_jill_wellington.jpg", "16": "venv/xmas_pics/16_pexels.jpg",
+    "17": "venv/xmas_pics/17_terri_cnudde.jpg", "18": "venv/xmas_pics/18_michelle_raponi.jpg",
+    "19": "venv/xmas_pics/19_wal_172619.jpg", "20": "venv/xmas_pics/20_ronalds.jpg",
+    "21": "venv/xmas_pics/21_alexandru_manole.jpg", "22": "venv/xmas_pics/22_sabine_kroschel.png",
+    "23": "venv/xmas_pics/23_couleur.jpg", "24": "venv/xmas_pics/24_jason_gillman.jpg"
+}
+
+
